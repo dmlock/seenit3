f5f656496c5563b058dd6c7897effe3b6bf9db92
dmlock <d.m.lock@hotmail.co.uk>
2021-04-09 18:21:46 +0100

    color scheme login wip

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
-import Sighting from "./Sighting";
 import Home from "./Home";
+import Login from "./Login";
+import Sighting from "./Sighting";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
   return (
     <Router>
       <div className="app">
       <Header />
         <Switch>
-          <Route path="/Sighting">
+        <Route path="/login">
+            <Login />
+          </Route>
+           <Route path="/Sighting">
             <Sighting />
           </Route>
           <Route path="/">
